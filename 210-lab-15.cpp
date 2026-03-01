f15c8ae4f16e79dfaab3c07d08b220a5ccaecb7e
dwoo27 <dwooed27@gmail.com>
2026-03-01 10:54:42 -0800

    10 min commit

--- 210-lab-15.cpp
+++ 210-lab-15.cpp
@@ -5,8 +5,47 @@
 using namespace std;
 
 const int MOVIES = 4;
+
+class Movie {
+    string title;
+    int year;
+    string writer;
+
+public:
+    void print(); //cout movie info
+
+    string getTitle() const; //accessors
+    int getYear() const;
+    string getWriter() const;
+
+    void setTitle(string); //mutators
+    void setYear(int);
+    void setWriter(string);
+};
+
+void finMovies(array<Movie, MOVIES>&);
+void coutMovies(array<Movie, MOVIES>&);
+
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
+void print() {
+    cout << "Movie: " << title << endl;
+    cout << "  Year released: " << yearReleased << endl;
+    cout << "  Screenwriter: " << screenWrtier << endl;
+}
+
+string getTitle() const {
+    return title;
+}
+
+int getYear() const [
+    return year;
+]
+string getWriter() const;
+
+void setTitle(string);
+void setYear(int);
+void setWriter(string);
